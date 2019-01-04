62f77050a8479a27dfa3ae7c6e748e22a855b6ba
duha921 <duhaalazzawi921@gmail.com>
2019-01-04 19:48:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,57 +1,27 @@
-/*
- * Express Example
- */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
 
-// Dependencies
-const express = require('express');
-const app = express();
-const Joi = require('joi');
-const jwt = require('jsonwebtoken');
-const hello =  require('./middleware/hello');
-const auth =  require('./middleware/auth');
-const usersRoutes = require('./routes/user');
-const userNotAuth = require('./routes/userNotAuth');
-//const login = require('./routes/login');
-
-const bookRoutes = require('./routes/books');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const compression = require('compression');
-const PORT = process.env.PORT || 5000
-const fileUpload = require('express-fileupload');
-//  Starting MongoDB connection
-//mongoose.connect('mongodb://admin:admin1234@ds127624.mlab.com:27624/camp', { useNewUrlParser: true });
-//mongoose.connect('mongodb://admin:ADMIN1992@ds063158.mlab.com:63158/bookstore',{useNewUrlParser: true})
-mongoose.connect('mongodb://admin:ADMIN1992@ds063158.mlab.com:63158/bookstore', {
-  useCreateIndex: true,
-  useNewUrlParser: true
-})
-
-//  To Check if the connection works fine or not
-mongoose.connection.on('connected', () => {
-  console.log('\x1b[36m%s\x1b[0m', 'mongo has been connected...');
-});
-
-// MiddleWare
-app.use(express.json());
-// Custom MiddleWare thats do nothing just to made the MiddleWare clear
-// app.use(hello);
-// For serving images and other static data
-app.use(fileUpload());
-
-app.use(express.static('public'));
-// Route MiddleWare for any route that start with (/api/user)
-app.use('/api/user',auth, usersRoutes);
-app.use('/api/userNotAuth', userNotAuth);
-app.use('/api/book',bookRoutes);
-//app.use('/api/login', login);
-// Home Router
-app.get('/', (req, res) => {
-  const token = jwt.sign({"name":"Hamdon", "age": 24}, 'key');
-  res.send(token);
-});
-
-// Starting the server
-app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`);
-});
+import Login from './components/login_page'
+export default class App extends Component {
+    state = {};
+  
+    render() {
+      return (
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route
+            path="/dashboard"
+            render={props => (
+              <BrowserRouter>
+                <Dashboard {...props} />
+              </BrowserRouter>
+            )}
+          />
+          <Redirect from="/" to="/login" />
+        </Switch>
+      );
+    }
+  }
+  
+ReactDOM.render(<Login/>,document.getElementById("root"))
